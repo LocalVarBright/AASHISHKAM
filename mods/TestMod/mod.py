9badb0bb9d4cccd8573e6488aac4eb89c855764c
LocalVarBright <garrissonchains@gmail.com>
2025-10-20 19:39:53 +0530

    Add Stuff 8 (really a mod update)

--- mods/TestMod/mod.py
+++ mods/TestMod/mod.py
@@ -1,15 +1,21 @@
 import urllib.request
 import os
+import time
 
 def downloadMusic():
-    os.makedirs("music", exist_ok=True) # Create the music folder in the same folder as mod.py
-    # NOTE: THE ABOVE LINE RUNS DIRECTLY FROM THE MODULE'S LOCATION, SO THE MUSIC FOLDER WILL BE CREATED IN THE SAME FOLDER AS mod.py
-
-    print("Downloading 'theme_1.ogg...")
-    urllib.request.urlretrieve("https://github.com/LocalVarBright/AASHISHKAM/raw/refs/heads/main/mods/TestMod/music/theme_1.ogg", "music/theme_1.ogg")
+    mod_dir = os.path.dirname(os.path.abspath(__file__)) # gets the path of the mod folder
+    musicPath = os.path.join(mod_dir, "music")
+    os.makedirs(musicPath, exist_ok=True) # Create the music folder in the same folder as mod.py
+    
+    print("Downloading 'theme_1.ogg'...")
+    if os.path.exists(os.path.join(musicPath, "theme_1.ogg")):
+        print("Already downloaded 'theme_1.ogg'.")
+    else:
+        urllib.request.urlretrieve("https://github.com/LocalVarBright/AASHISHKAM/raw/refs/heads/main/mods/TestMod/music/theme_1.ogg", os.path.join(musicPath, "theme_1.ogg"))
     print()
 
 def start(doDialogText, doDialogSlow, askChoice, askNum, doDialogChoice, doTimedQuestion, doTimedAttack, doTimedSpam, printGraphic, getPrompt, playSong, stopSong, timeControl, pgFilter, saveFile, saveGame, curSaveName, soundImportSuccesful):
+    # If music was succesfully loaded, 'soundImportSuccesful' will be set to True. Use this variable when handling sound.
     if soundImportSuccesful:
         downloadMusic()
         playSong("mods/TestMod/music/theme_1.ogg", looping=True)
@@ -20,13 +26,16 @@ def start(doDialogText, doDialogSlow, askChoice, askNum, doDialogChoice, doTimed
     if soundImportSuccesful: doDialogText("Isn't the music nice?")
     print()
     doDialogText("We shall test the capabilities mods can bring us.")
-    choice = doDialogChoice("Do you like this mod so far?", ["Yes", "No"])
+    
+    # Demonstration of choice
+    choice = doDialogChoice("Do you like this mod so far?", choices=["Yes", "No"])
     
     if choice == 1:
         doDialogText("Glad to hear that,# Though this mod has just started.")
     else:
         doDialogText("I see.# Well the mod just started, so do stick around.")
     
+    # 'print()' function to leave gaps in the text for better format.
     print()
 
     doDialogText("Let's talk about how 2.0 is coming along so far.")
@@ -38,6 +47,22 @@ def start(doDialogText, doDialogSlow, askChoice, askNum, doDialogChoice, doTimed
     doDialogText("2.0 also brings the ability to create MODS,# such as one you are playing right now.")
     doDialogText("Chapters in Aashishkam take a similar approach to how modding works,# so if you want a reference,# look at the chapter modules in the AppData folder.")
     doDialogText("AASHISHKAM is stored in %AppData%/AASHISHKAM now.")
+
+    musicChoice = doDialogChoice("Would you like to listen to the music further?", choices=["Yes", "No"])
+
+    if musicChoice == 1:
+        doDialogText("Very well.# Press ENTER when you would like to stop.")
+        input("Press ENTER to finish the mod.")
+    
+    doDialogText("Very well.# Thank you for testing out this test mod.")
+    doDialogText("Also yeah this test mod is also BETA.# I plan to demonstrate many features of mods in the future through this mod.")
+    print()
+    doDialogText("If you ran into any errors,# please do report them back to the developer, aka me.", afterdelay=1.1)
+
+    print()
+    doDialogChoice("Well,# that's it.# Goodbye.", choices=["Goodbye."])
+
+    time.sleep(5)
 
     if soundImportSuccesful:
         stopSong()
