31690d6c1937c4697f6eb0d9410ee4502cd01693
LocalVarBright <garrissonchains@gmail.com>
2026-02-14 16:30:47 +0530

    2.0 BETA-16 (5)
    ... dont ask. it works now.

--- assets/chapters/chapter5.py
+++ assets/chapters/chapter5.py
@@ -1,12 +1,21 @@
+from datetime import date
+
+bebruary = date(2026, 2, 15)
+today = date.today()
+
 def start(funcs):
 
     doDialogText = funcs['doDialogText']
 
-    doDialogText("Loading Chapter 1.#.#.#", afterdelay=3)
+    doDialogText("Loading Chapter 5.#.#.#", afterdelay=3)
     print()
 
-    doDialogText("Go study for Brilliant's Scholarship exam you twat")
-    doDialogText("Even I'M studying for once.")
+    if today >= bebruary:
+        doDialogText("SIDDUMON:# Sorry, but i was making sure you were actually studying for the scholarship exam.")
+        doDialogText("          Chapter 5 is not done yet")
+    else:
+        doDialogText("SIDDUMON:# Go study for Brilliant's Scholarship exam you twat")
+        doDialogText("           Even I'M studying for once.")
     doDialogText(" - Siddumon")
 
     
